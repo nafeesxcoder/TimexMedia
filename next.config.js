e1bf2fd3cd687407c4023eb3e1e32ea4ab942cd1
nafees  Ahmad <nafeesahadbly@gmail.com>
2026-07-08 09:38:57 -0700

    add manifest and sw file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -19,4 +26,4 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
